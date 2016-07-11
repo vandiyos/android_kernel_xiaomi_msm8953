bc244c66f64e8c4e2a895e8520053ec4adeb2fee
Naseer Ahmed <naseer@codeaurora.org>
2016-07-11 15:48:03 -0400

    FORCE FAST CHARGE: allow max 2500ma draw from normal charger!
    Tested and works great! unlish the power!
    
    (with small fix for MIDO)

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -442,8 +442,11 @@ module_param_named(
 	main_chg_icl_percent, smbchg_main_chg_icl_percent,
 	int, S_IRUSR | S_IWUSR
 );
-
+#ifdef CONFIG_FORCE_FAST_CHARGE
+static int smbchg_default_hvdcp_icl_ma = 2500;
+#else
 static int smbchg_default_hvdcp_icl_ma = 1800;
+#endif
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
@@ -455,7 +458,11 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 1800;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+static int smbchg_default_dcp_icl_ma = 2500;
+#else
+static int smbchg_default_dcp_icl_ma = 2000;
+#endif
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
@@ -1394,6 +1401,8 @@ static int dc_ilim_ma_table_8996[] = {
 	2200,
 	2300,
 	2400,
+	2500,
+	2600,
 };
 
 static const int fcc_comp_table_8994[] = {
@@ -1401,6 +1410,12 @@ static const int fcc_comp_table_8994[] = {
 	700,
 	900,
 	1200,
+	1500,
+	1600,
+	1800,
+	2000,
+	2500,
+	2600,
 };
 
 static const int fcc_comp_table_8996[] = {
