f71fa39102baf8b822dab688272684063f7fd109
Douglas RAILLARD <douglas.raillard@arm.com>
2019-06-18 22:56:35 +0530

    BACKPORT: sched/cpufreq: sugov_cpu_is_busy for shared policy
    Allow using sugov_cpu_is_busy() for policy from sugov_update_shared().
    This means that the heuristic needs to return stable results across
    multiple calls for a given CPU, even if there has been no utilization
    change since last call.
    
    sugov_cpu_is_busy() currently both checks business status and updates
    the counters, so let's decouple the two actions.
    
    Signed-off-by: Douglas RAILLARD <douglas.raillard@arm.com>
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -203,8 +203,15 @@ static bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu)
 	sg_cpu->saved_idle_calls = idle_calls;
 	return ret;
 }
+
+static void sugov_cpu_is_busy_update(struct sugov_cpu *sg_cpu)
+{
+	unsigned long idle_calls = tick_nohz_get_idle_calls();
+	sg_cpu->saved_idle_calls = idle_calls;
+}
 #else
 static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
+static void sugov_cpu_is_busy_update(struct sugov_cpu *sg_cpu) {}
 #endif /* CONFIG_NO_HZ_COMMON */
 
 /**
@@ -568,8 +575,6 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 	}
 }
 
-
-
 static void sugov_update_single(struct update_util_data *hook, u64 time,
 				unsigned int flags)
 {
@@ -594,6 +599,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		return;
 
 	busy = sugov_cpu_is_busy(sg_cpu);
+	sugov_cpu_is_busy_update(sg_cpu);
 
 	if (flags & SCHED_CPUFREQ_DL) {
 		next_f = policy->cpuinfo.max_freq;
