38620b484d2931f90d08b41ef4dee47f7efce031
kondors1995 <normandija1945@gmail.com>
2019-06-21 14:14:04 +0000

    Revert "Mido: Import K-LAPSE 4.3"
    This reverts commit 785a8e4c50f9d815f8e3fe33ecb4b661688f45fe.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -60,9 +60,6 @@
 #include "mdp3_ctrl.h"
 
 #include "mdss_livedisplay.h"
-#ifdef CONFIG_KLAPSE
-#include "klapse.h"
-#endif
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -344,9 +341,6 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		mdss_fb_set_backlight(mfd, bl_lvl);
 		mutex_unlock(&mfd->bl_lock);
 	}
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 }
 
 static struct led_classdev backlight_led = {
